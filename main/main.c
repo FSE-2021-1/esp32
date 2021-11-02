09f408734e5fd38ba3bfb71f615ed1dbfbcdc44f
Rhuan Queiroz <rhuancarlos.queiroz@gmail.com>
2021-11-01 21:37:26 -0300

    Adicionando configuração WIFI e teste HTTP

--- main/main.c
+++ main/main.c
@@ -9,19 +9,56 @@
 #include <stdio.h>
 #include "sdkconfig.h"
 #include "freertos/FreeRTOS.h"
+#include "freertos/semphr.h"
 #include "freertos/task.h"
 #include "driver/gpio.h"
 #include "dht11.h"
+#include "nvs_flash.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "esp_http_client.h"
+#include "esp_log.h"
+#include "wifi.h"
+#include "http_client.h"
+
+xSemaphoreHandle conexaoWifiSemaphore;
+
+void RealizaHTTPRequest(void * params)
+{
+  while(true)
+  {
+    if(xSemaphoreTake(conexaoWifiSemaphore, portMAX_DELAY))
+    {
+      ESP_LOGI("Main Task", "Realiza HTTP Request");
+      http_request();
+      https_request();
+    }
+  }
+}
 
 void app_main(void)
 {
-    DHT11_init(GPIO_NUM_4);
+    // Inicializa o NVS
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+      ESP_ERROR_CHECK(nvs_flash_erase());
+      ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
+    
+    conexaoWifiSemaphore = xSemaphoreCreateBinary();
+    wifi_start();
+
+    xTaskCreate(&RealizaHTTPRequest,  "Processa HTTP", 4096, NULL, 1, NULL);
+    // TODO Remove HTTP request
+
+    /* DHT11_init(GPIO_NUM_4);
 
     while(1) {
         printf("Temperature is %d \n", DHT11_read().temperature);
         printf("Humidity is %d\n", DHT11_read().humidity);
         printf("Status code is %d\n", DHT11_read().status);
         vTaskDelay(1000 / portTICK_PERIOD_MS);
-    }
+    } */
 }
 
